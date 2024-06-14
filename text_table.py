2e598981295bbbe1c46209729a93a6ac540879f0
jkirn <jimkirn@outlook.com>
2024-06-14 16:15:31 -0500

    cleaned up webpage format

--- text_table.py
+++ text_table.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-# Run as: html-table.py
-# The script requires file data.txt which is created by scan_lan3.py
-# It expects a comma-separated input file to parse into an html table base web page.
-# It assumes that the column headers are located in the first row.
-# 
+# Run as: python3 html-table.py
+# The script requires file 'data.txt' which is created by 'scan_lan3.py'
+# It expects a comma-separated input file (data.txt) which it parses into a html web page file (html-table.html).
+# It assumes that the column headers are located in the first row of the 'data.txt' file.
+# General web page formating from example: https://www.geeksforgeeks.org/how-to-fix-the-width-of-columns-in-the-table/
 
 import sys
 import time
@@ -12,7 +12,7 @@ import time
 infile = '/dev/shm/data.txt'
 outfile = '/dev/shm/html-table.html'
 
-# Process open of input file
+# Process opening of input file (data.txt)
 try:
    with open(infile,'r') as filein:
       # read all the data from the input file
@@ -22,54 +22,79 @@ except IOError:
    print('File {0} not accessible' .format(infile) )
    exit()
 #
-# Start creating the Output file data
-# Create top of Page
+# Start creating the HTML Output file data (html-table.html)
+## Create top of Page
 table = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n'
 table += '<html xmlns="http://www.w3.org/1999/xhtml">\n'
-table += '<head>\n'
 table += '<title>SCAN_LAN REPORT</title>\n'
-# Create table CSS
-table += '<style>\n'
-table += 'table {\n'
-table += '\ttable-layout: auto;\n'
-table += '\twidth: 1300px;\n'
+
+## Create table CSS
+table += '<head>\n'
+table += '\t<style>\n'
+table += '\t\ttable {\n'
+table += '\t\t\tmargin=left: auto;\n'
+table += '\t\t\tmargin=right: auto;\n'
+table += '\t\t\tfont-size: 20px;\n'
+table += '\t\t\theight: 100%;\n'
+table += '\t\t\ttable-layout: fixed;\n'
 table += '}\n'
-table += '</style>\n'
+#
+table += '\t\ttd {\n'
+table += '\t\t\tborder: 1px solid black;\n'
+table += '\t\t\ttext-align: center;\n'
+table += '\t\t\tpadding: 10px;\n'
+table += '\t\t\t}\n'
+#
+table += '\t\ttr:nth-child(even) {\n'
+table += '\t\t\tbackground-color: #00cf45;\n'
+table += '\t\t}\n'
+# 
+table += '\t\th1 {\n'
+table += '\t\t\tcolor: green;\n'
+table += '\t\t}\n'
+#
+table += '\t</style>\n'
 table += '</head>\n'
-# Start the Body
+
+## Start the Body
 table += '<body>\n'
-# Start the Table
-table += "<table>\n"
-# Create the table's column headers
+table += '\t<center>\n'
+table += '\t\t<h1>ScanLan Report</h1>\n'
+
+## Put in a Report date+time
+localtime = time.asctime( time.localtime(time.time()) )
+table += '\t\t<h2>{0}\n'.format(localtime)
+table += '\t\t</h2>\n'
+
+## Start the Table
+table += "\t\t<table>\n"
+## Create the table's column headers
 header = data[0].split(",")
-# Create all header row data
-table += "  <tr>\n"
+
+## Create the header row data
+table += "\t\t\t<tr>\n"
 for column in header:
-    table += " <th>{0}</th>\n".format(column.strip())
-table += " </tr>\n"
-# Create the table's row data
+    table += "\t\t\t\t<td>{0}</td>\n".format(column.strip())
+table += "\t\t\t</tr>\n"
+
+## Create the table's row data
 mycnt = 0
 for line in data[1:]:
     mycnt += 1
     row = line.split(",")
-    table += "  <tr>\n"
+    table += "\t\t\t<tr>\n"
     for column in row:
-        table += " <td>{0}</td>\n".format(column.strip())
-    table += "  </tr>\n"
-table += "</table>\n"
+        table += "\t\t\t\t<td>{0}</td>\n".format(column.strip())
+    table += "\t\t\t</tr>\n"
+table += "\t\t</table>\n"
+table += "\t</center>\n"
+table += '</body>'
 
-# Show total number of MAC addresses found
+## Show total number of MAC addresses found
 table += '<br>\n'
 table += '<b>Total MAC addesses found {0}</b>\n' .format(mycnt)
 
-# Put in a Report date
-localtime = time.asctime( time.localtime(time.time()) )
-
-table += '<br>\n<br>\n'
-table += '<b>{0}</b>\n' .format(localtime)
-table += '</body>'
-
-# Write output data to file!
+## Write output data to file (html-table.html)!
 try:
    with open (outfile,'w') as fileout:
       fileout.writelines(table)

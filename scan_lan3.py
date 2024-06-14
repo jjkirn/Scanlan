d574adc18dc4f653ff95481b679455ffa2981ba6
jkirn <jimkirn@outlook.com>
2024-06-13 21:58:20 -0500

    updated scan_lan3

--- scan_lan3.py
+++ scan_lan3.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
+#!usr/bin/python3
 #
 # JJKirn 2/8/2020, updated 6/1/2024
 #
-
 from builtins import *
 from scapy.all import *
 from socket import inet_aton,inet_ntoa
@@ -11,18 +10,19 @@ import json
 import codecs
 import getopt,sys
 import requests
+import time
 
-myMAC = {}
+myMAC = {}	## Global empty dictionary
+version = "3.3"
+subnet = "192.168.1.0/24"  ## subnet to scan - change this to match your subnet
 
-# Create dictionary  of my MAC Address to Descriptions (change this to match your system)
-#
-# Read MAC address supplementary data file (mac.txt) to populate the dictionary
-# Format of file is MACaddress,Detail supplementary info for that MAC, optional company 
-
+# Create a dictionary of my MAC Address to Descriptions 
+# Read MAC address supplementary data file (mac.txt) to populate the dictionary (change mac.txt to match your system)
+# Format of the file is MACaddress, Detail supplementary info for that MAC, optional company 
 def read_mac_file(fpath):
 	cnt = 1
 
-	# print('fpath = {}'.format(fpath))
+	# print('fpath = {}'.format(fpath))	## Debug
 
 	try:
 		with open (fpath) as f:
@@ -43,7 +43,6 @@ def read_mac_file(fpath):
 	except IOError:
 		print('File {0} not accessible' .format(fpath) )
 
-	#
 	print('{0} MAC addresses read in from detail data File ({1})' .format(cnt, fpath) )
 
 	# disable scapy promiscuous mode (not sure this is needed)
@@ -51,25 +50,44 @@ def read_mac_file(fpath):
 
 	return(True)
 
-# Mac address to lookup vendor - REF: https://www.youtube.com/watch?v=nhC3paE_YPk,  vendor=company
+# Mac address to vendor (company) lookup - REF: https://www.youtube.com/watch?v=nhC3paE_YPk -> vendor=company
 def get_co(macaddr):
-	# print('macaddr: {}'.format(macaddr))
+	# print('macaddr: {}'.format(macaddr))  ## Debug
 
 	# MAC vendor API base url, you can also use https if you need
-	url = f'https://www.macvendorlookup.com/api/v2/{macaddr}'
+	#url = f'http://www.macvendorlookup.com/api/v2/{macaddr}'   ## kept timing out 502 error - I think they have daily limits
+	url = f'https://api.maclookup.app/v2/macs/{macaddr}'  ## this site is rate limited, doesnt look like daily limits
 	
-	# print('Request: {}'.format(url))
+	# print('Request: {}'.format(url)) ## Debug
 	
 	try:
-		vendor = requests.get(url).json()
-		vendor = vendor[0]['company']
+		# rate limited, try 1 sec, for site https://api.maclookup.app/
+		time.sleep(1) # Wait for 1 sec
 		
-	except:
-		vendor = 'none'
+		r = requests.get(url, timeout=(2)).json()
+		# print('r = {}'.format(r))  ## Debug
+		
+		# Below is dependent on the values (JSON) returned from the url. If you change the url, you may need to change the below
+		company = r['company']
+		# print('company = {}'.format(company)) ## Debug
+		
+		if company == '':
+			company = 'none found'
+			
+		if r['success'] == False:
+			print('Site is rate limiting - Too Many Requests: {}'.format(url) )
+			
+	except requests.exceptions.Timeout:
+		print('HTTP Connection Error to site {} timed out'.format(url))
+		company = 'none'
 
-	# print("vendor = {}".format(vendor))
+	# except requests.exceptions.ConnectionError as errh:
+	except requests.exceptions.ConnectionError:
+		print('HTTP Connection Error to site {} timed out'.format(url))
+		company = 'none'
 	
-	return vendor
+	# print("company = {}".format(company)) ## Debug	
+	return company
 
 # Convert dotted decimal IP to long - ex:ip2long('192.168.1.1'):
 def ip2long(ip):
@@ -90,44 +108,56 @@ def do_arping(co):
 	print("Processing...")
 
 	# change this to match your subnet
-	ans,unans = arping("192.168.1.0/24", verbose=0)
+	mysub = subnet
+	ans,unans = arping(mysub, verbose=0, timeout=15)
 
-	# cnt = 0
+	cnt = 1
 
-	#print("----------------------------------------------------------------------------------------------")
-	#print("MAC Address" + "\t\t" + "IP Address" + "\t" + "Description" )
+	# append data from arping to my_list
 	for s,r in ans:
+		
 		if co == True:
+			print('Getting company information from site for item {}'.format(cnt))
 			try:
 				#print('{0}\t{1}\t== {2}\t** {3}' .format( r[Ether].src,s[ARP].pdst,myMAC[r[Ether].src],get_co(r[Ether].src)) )
 				my_list.append( (ip2long(s[ARP].pdst), r[Ether].src, myMAC[r[Ether].src], get_co(r[Ether].src)) )
+				# below is a debug print
+				# print('{}, {}, {}, {}'.format(ip2long(s[ARP].pdst), r[Ether].src, myMAC[r[Ether].src], get_co(r[Ether].src)))
+				
 			except KeyError:
 				print('{0}\t{1}\t== MAC Address not found in MAC Table, Please add!' .format(r[Ether].src,s[ARP].pdst) )
 		else:
 			try:
 				#print('{0}\t{1}\t== {2}' .format( r[Ether].src,s[ARP].pdst,myMAC[r[Ether].src] ) )
 				my_list.append( (ip2long(s[ARP].pdst), r[Ether].src, myMAC[r[Ether].src]) )
+				
 			except KeyError:
 				print('{0}\t{1}\t== MAC Address not found in MAC Table, Please add!' .format(r[Ether].src,s[ARP].pdst) )
-		# cnt += 1
+		cnt += 1
 
-   	# sort by IP address
+   	# sort my_list by IP address
 	for aTuple in my_list:
 		my_list.sort(key=lambda tup:tup[0])
 
 	# convert long IP address to dotted decimal (done in above for loop)
 
-	# print results
+	# print my_list sorted results
 	cnt = 0
 	myString = ''
 	print("----------------------------------------------------------------------------------------------")
+	
 	# Print the column headers
+	t1 = "IP Address"
+	t2 = "MAC Address"
+	t3 = "Description"
+	t4 = "Company"
+	
 	if co == True:
-		myString = "IP Address" + "," + "MAC Address" + "," + "Description" + "," + "** Company Info\n"
+		myString = "IP Address" + "," + "MAC Address" + "," + "Description" + "," + "Company\n"
 	else:
 		myString = "IP Address" + "," + "MAC Address" + "," + "Description\n"
 
-	# Print the row data
+	# print the row data
 	for aTuple in my_list:
 		if co == True:
 			myString += ('{0},{1},{2},{3}\n' .format(long2ip(aTuple[0]),aTuple[1],aTuple[2],aTuple[3]) )
@@ -135,11 +165,13 @@ def do_arping(co):
 			myString += ('{0},{1},{2}\n' .format(long2ip(aTuple[0]),aTuple[1],aTuple[2]) )
 
 		cnt += 1
+	
 	print(myString)
+	# Write data to file in comma seperated format
 	write_string(myString)
 	print('{0} Hosts found!' .format(cnt) )
 
-	#print('*******************************')
+	#print('*******************************')  ## Debug
 	return True
 
 # Write string to file
@@ -154,6 +186,7 @@ def write_string(my_str):
 		myret = 0
 
 	f2.close
+	print('Wrote data to file: {}'.format(mystrpath))
 	return myret
 
 def usage():
@@ -186,7 +219,7 @@ def main(argv):
 			co = True
 
 		if opt in ('-v','--version'):
-			print('Version 3.1')
+			print('Version {}'.format(version))
 			sys.exit(2)
 
 	read_mac_file(fpath)
